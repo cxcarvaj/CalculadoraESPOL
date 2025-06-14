f23145360bdc381396cf65c62db005ff5c5f08dc
cxcarvaj <cxcarvaj@gmail.com>
2025-06-14 09:55:58 -0500

    Initial Commit

---
+++ CalculadoraESPOL/CalculadoraESPOLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraESPOLApp.swift
+//  CalculadoraESPOL
+//
+//  Created by Carlos Xavier Carvajal Villegas on 14/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraESPOLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
